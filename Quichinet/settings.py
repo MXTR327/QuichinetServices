8a64617f15b85a497811b77d074bfd0d4efc0e2d
Maxter <111721078+MXTR327@users.noreply.github.com>
2024-02-07 15:16:50 -0500

    commit 26

--- Quichinet/settings.py
+++ Quichinet/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'gestionClientes/static')
+    os.path.join(BASE_DIR, 'Quichinet/static')
 ]
 
 # Default primary key field type
