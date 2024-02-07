d2c46b3908039fa98a2f47c588fbd7f8d3fc41ab
Maxter <111721078+MXTR327@users.noreply.github.com>
2024-02-07 13:03:12 -0500

    commit 4

--- Quichinet/settings.py
+++ Quichinet/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'Quichinet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
