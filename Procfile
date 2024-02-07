e17bcf2c5c7cc3492509e6d7ae65e46474628e5a
Maxter <111721078+MXTR327@users.noreply.github.com>
2024-02-07 13:59:15 -0500

    commit 11

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Quichinet.wsgi --log-file -
--- Quichinet/settings.py
+++ Quichinet/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'gestionClientes',
 ]
 
@@ -108,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gestionClientes/static')
