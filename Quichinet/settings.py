89b0e45d7ba7bb7b6cc30f2ed9dc3f47e0f0e913
Maxter <111721078+MXTR327@users.noreply.github.com>
2024-02-08 11:00:17 -0500

    commit 39

--- Quichinet/settings.py
+++ Quichinet/settings.py
@@ -13,11 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1r&ev+^#5d1pqrr(8l)iamo1954dk8p0yj_d=9t#nr$4+rgg@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #IMPORTANTES ---
 
-ALLOWED_HOSTS = ['quichinetservices-production.up.railway.app']
+ALLOWED_HOSTS = ['*', 'quichinetservices-production.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://quichinetservices-production.up.railway.app']
 
@@ -113,9 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'Quichinet/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gestionClientes/static/')
