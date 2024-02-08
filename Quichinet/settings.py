1c33ec7dd4c77fa85dbbda59c28015bf9fa5844a
Maxter <111721078+MXTR327@users.noreply.github.com>
2024-02-08 09:50:07 -0500

    commit 34

--- Quichinet/settings.py
+++ Quichinet/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-1r&ev+^#5d1pqrr(8l)iamo1954dk8p0yj_d=9t#nr$4+rgg@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'web-production-da13.up.railway.app']
+ALLOWED_HOSTS = ['https://quichinetservices-production.up.railway.app']
 
 
 # Application definition
